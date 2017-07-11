e945507e9cc0c76e9315f43e365d0a72b7f511ee
Jeremie Desautels <desautelsj@hotmail.com>
2017-07-11 16:20:45 -0400

    Xml docs

--- Source/Picton/Extensions/AssemblyExtensions.cs
+++ Source/Picton/Extensions/AssemblyExtensions.cs
@@ -8,11 +8,21 @@ namespace Picton.Extensions
 	{
 		#region PUBLIC EXTENSION METHODS
 
+		/// <summary>
+		/// Returns a boolean indicating if the assembly has been compiled in Debug mode.
+		/// </summary>
+		/// <param name="assembly">The assembly</param>
+		/// <returns>true if the assembly was compiled in Debug mode; false otherwise</returns>
 		public static bool IsDebugConfiguration(this Assembly assembly)
 		{
 			return IsAssemblyConfiguration(assembly, "Debug");
 		}
 
+		/// <summary>
+		/// Returns a boolean indicating if the assembly has been compiled in Release mode.
+		/// </summary>
+		/// <param name="assembly">The assembly</param>
+		/// <returns>true if the assembly was compiled in Release mode; false otherwise</returns>
 		public static bool IsReleaseConfiguration(this Assembly assembly)
 		{
 			return IsAssemblyConfiguration(assembly, "Release");
