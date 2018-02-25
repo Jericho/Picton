4627ff1ac3457ee363decae85b0150c0d521af9a
Jeremie Desautels <desautelsj@hotmail.com>
2018-02-25 17:50:55 -0500

    XML comments

--- Source/Picton/Extensions/CloudBlobClientExtensions.cs
+++ Source/Picton/Extensions/CloudBlobClientExtensions.cs
@@ -5,10 +5,22 @@ using System.Threading.Tasks;
 
 namespace Picton
 {
+	/// <summary>
+	/// Contains extension methods for the <see cref="CloudBlobClient"/> class.
+	/// </summary>
 	public static class CloudBlobClientExtensions
 	{
 		#region PUBLIC EXTENSION METHODS
 
+		/// <summary>
+		/// Lists the containers in a storage account.
+		/// </summary>
+		/// <param name="blobClient">The blob client</param>
+		/// <param name="prefix">Prefix</param>
+		/// <param name="listingDetails">Specifies which details to include when listing the containers</param>
+		/// <param name="maxResults">The maximum number of containers to include in the result</param>
+		/// <param name="cancellationToken">Th cancellation token</param>
+		/// <returns>The list of containers</returns>
 		public static async Task<IEnumerable<CloudBlobContainer>> ListContainersAsync(this CloudBlobClient blobClient, string prefix = null, ContainerListingDetails listingDetails = ContainerListingDetails.Metadata, int? maxResults = null, CancellationToken cancellationToken = default(CancellationToken))
 		{
 			var continuationToken = (BlobContinuationToken)null;
