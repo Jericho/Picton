2278f218f2b1077f95f4f43c7b2a1eaff7fbc19a
Jeremie Desautels <desautelsj@hotmail.com>
2018-02-26 15:05:58 -0500

    XML comments

--- Source/Picton/Extensions/CloudBlobDirectoryExtensions.cs
+++ Source/Picton/Extensions/CloudBlobDirectoryExtensions.cs
@@ -5,10 +5,22 @@ using System.Threading.Tasks;
 
 namespace Picton
 {
+	/// <summary>
+	/// Contains extension methods for the <see cref="CloudBlobDirectory"/> class.
+	/// </summary>
 	public static class CloudBlobDirectoryExtensions
 	{
 		#region PUBLIC EXTENSION METHODS
 
+		/// <summary>
+		/// Lists the blobs in a blob directory (AKA virtual directory)
+		/// </summary>
+		/// <param name="blobFolder">The directory</param>
+		/// <param name="includeSubFolders">Indicates whether to list blobs in a flat listing or to list blobs hierarchically, by virtual directory.</param>
+		/// <param name="listingDetails">Specifies which details to include when listing the blobs</param>
+		/// <param name="maxResults">The maximum number of blobs to include in the result</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <returns>The list of blobs</returns>
 		public static async Task<IEnumerable<IListBlobItem>> ListBlobsAsync(this CloudBlobDirectory blobFolder, bool includeSubFolders = false, BlobListingDetails listingDetails = BlobListingDetails.Metadata, int? maxResults = null, CancellationToken cancellationToken = default(CancellationToken))
 		{
 			var continuationToken = (BlobContinuationToken)null;
