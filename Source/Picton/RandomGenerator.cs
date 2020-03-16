ae5300c39b8fdadcbae189c44463191f3cac2871
Jericho <desautelsj@hotmail.com>
2020-03-16 13:25:24 -0400

    Fix XML comments

--- Source/Picton/RandomGenerator.cs
+++ Source/Picton/RandomGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Security.Cryptography;
 
 namespace Picton
@@ -11,8 +11,8 @@ namespace Picton
 		/// This method simulates a roll of the dice. The input parameter is the
 		/// number of sides of the dice.
 		/// </summary>
-		/// <param name="numberSides">Number of sides of the dice</param>
-		/// <returns></returns>
+		/// <param name="numberSides">Number of sides of the dice.</param>
+		/// <returns>A random value.</returns>
 		/// <remarks>
 		/// From: https://msdn.microsoft.com/en-us/library/system.security.cryptography.rngcryptoserviceprovider.aspx
 		/// </remarks>
@@ -106,12 +106,12 @@ namespace Picton
 		private static bool IsFairRoll(byte roll, byte numSides)
 		{
 			// There are MaxValue / numSides full sets of numbers that can come up
-			// in a single byte.  For instance, if we have a 6 sided die, there are
-			// 42 full sets of 1-6 that come up.  The 43rd set is incomplete.
+			// in a single byte. For instance, if we have a 6 sided die, there are
+			// 42 full sets of 1-6 that come up. The 43rd set is incomplete.
 			int fullSetsOfValues = byte.MaxValue / numSides;
 
 			// If the roll is within this range of fair values, then we let it continue.
-			// In the 6 sided die case, a roll between 0 and 251 is allowed.  (We use
+			// In the 6 sided die case, a roll between 0 and 251 is allowed. (We use
 			// < rather than <= since the = portion allows through an extra 0 value).
 			// 252 through 255 would provide an extra 0, 1, 2, 3 so they are not fair
 			// to use.
