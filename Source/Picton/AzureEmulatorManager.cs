2fa125cbdccd4864f6bdee9266a62858ed90d730
Jericho <desautelsj@hotmail.com>
2022-04-03 15:21:33 -0400

    Each emulator version can indicate whether they require "elevated"

--- Source/Picton/AzureEmulatorManager.cs
+++ Source/Picton/AzureEmulatorManager.cs
@@ -68,13 +68,16 @@ namespace Picton
 			/// </summary>
 			public string StopParameters { get; private set; }
 
-			public EmulatorVersionInfo(int version, IEnumerable<string> processNames, string executablePath, string startParameters, string stopParameters)
+			public bool RequireElevated { get; private set; }
+
+			public EmulatorVersionInfo(int version, IEnumerable<string> processNames, string executablePath, string startParameters, string stopParameters, bool requireElevated)
 			{
 				Version = version;
 				ProcessNames = processNames.ToArray();
 				ExecutablePath = executablePath;
 				StartParameters = startParameters;
 				StopParameters = stopParameters;
+				RequireElevated = requireElevated;
 			}
 		}
 
@@ -89,12 +92,12 @@ namespace Picton
 
 		static AzureEmulatorManager()
 		{
-			_storageEmulatorVersions.Add(new EmulatorVersionInfo(2, new[] { "DSService" }, @"C:\Program Files\Microsoft SDKs\Windows Azure\Emulator\csrun.exe", "/devstore:start", "/devstore:stop"));
-			_storageEmulatorVersions.Add(new EmulatorVersionInfo(3, new[] { "WAStorageEmulator", "WASTOR~1" }, @"C:\Program Files (x86)\Microsoft SDKs\Windows Azure\Storage Emulator\WAStorageEmulator.exe", "start", "stop"));
-			_storageEmulatorVersions.Add(new EmulatorVersionInfo(4, new[] { "AzureStorageEmulator" }, @"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe", "start", "stop"));
+			_storageEmulatorVersions.Add(new EmulatorVersionInfo(2, new[] { "DSService" }, @"C:\Program Files\Microsoft SDKs\Windows Azure\Emulator\csrun.exe", "/devstore:start", "/devstore:stop", false));
+			_storageEmulatorVersions.Add(new EmulatorVersionInfo(3, new[] { "WAStorageEmulator", "WASTOR~1" }, @"C:\Program Files (x86)\Microsoft SDKs\Windows Azure\Storage Emulator\WAStorageEmulator.exe", "start", "stop", false));
+			_storageEmulatorVersions.Add(new EmulatorVersionInfo(4, new[] { "AzureStorageEmulator" }, @"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe", "start", "stop", false));
 
-			_documentDbEmulatorVersions.Add(new EmulatorVersionInfo(0, new[] { "DocumentDB.Emulator" }, @"C:\Program Files\DocumentDB Emulator\DocumentDB.Emulator.exe", string.Empty, "/shutdown"));
-			_documentDbEmulatorVersions.Add(new EmulatorVersionInfo(1, new[] { "DocumentDB.GatewayService" }, @"C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe", string.Empty, "/shutdown"));
+			_documentDbEmulatorVersions.Add(new EmulatorVersionInfo(0, new[] { "DocumentDB.Emulator" }, @"C:\Program Files\DocumentDB Emulator\DocumentDB.Emulator.exe", string.Empty, "/shutdown", true));
+			_documentDbEmulatorVersions.Add(new EmulatorVersionInfo(1, new[] { "DocumentDB.GatewayService" }, @"C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe", string.Empty, "/shutdown", true));
 		}
 
 		#endregion
@@ -109,7 +112,7 @@ namespace Picton
 			// The storage emulator process completes quickly therefore no need to set a wait timeout
 			var waitTimeout = TimeSpan.Zero;
 
-			EnsureEmulatorIsStarted(_storageEmulatorVersions, false, waitTimeout);
+			EnsureEmulatorIsStarted(_storageEmulatorVersions, waitTimeout);
 		}
 
 		/// <summary>
@@ -128,7 +131,7 @@ namespace Picton
 			// The DocumentDb emulator process never seems to complete (I don't know why), therefore we must set a reasonable wait timeout
 			var waitTimeout = TimeSpan.FromSeconds(20);
 
-			EnsureEmulatorIsStarted(_documentDbEmulatorVersions, true, waitTimeout);
+			EnsureEmulatorIsStarted(_documentDbEmulatorVersions, waitTimeout);
 		}
 
 		/// <summary>
@@ -143,7 +146,7 @@ namespace Picton
 
 		#region PRIVATE METHODS
 
-		private static void EnsureEmulatorIsStarted(IEnumerable<EmulatorVersionInfo> emulatorVersions, bool elevated, TimeSpan waitTimeout)
+		private static void EnsureEmulatorIsStarted(IEnumerable<EmulatorVersionInfo> emulatorVersions, TimeSpan waitTimeout)
 		{
 			var found = false;
 
@@ -159,7 +162,7 @@ namespace Picton
 						count += Process.GetProcessesByName(processName).Length;
 					}
 
-					if (count == 0) LaunchProcess(emulatorVersion.ExecutablePath, emulatorVersion.StartParameters, elevated, waitTimeout);
+					if (count == 0) LaunchProcess(emulatorVersion.ExecutablePath, emulatorVersion.StartParameters, emulatorVersion.RequireElevated, waitTimeout);
 					found = true;
 					break;
 				}
@@ -196,21 +199,19 @@ namespace Picton
 		{
 			var start = new ProcessStartInfo
 			{
-#if NETFULL
+				CreateNoWindow = true,
 				WindowStyle = ProcessWindowStyle.Hidden,
-#endif
 				UseShellExecute = false,
 				Arguments = arguments,
 				FileName = fileName
 			};
 
-#if NETFULL
 			if (elevated)
 			{
 				start.Verb = "runas";
 				start.UseShellExecute = true;
 			}
-#endif
+
 			var exitCode = 0;
 
 			using (var proc = new Process { EnableRaisingEvents = true, StartInfo = start })
