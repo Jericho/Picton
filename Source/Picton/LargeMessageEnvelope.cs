a5e19f6ca0633f20a36cf8425c6626e2344affdf
Jericho <desautelsj@hotmail.com>
2026-03-30 20:48:01 -0400

    Add XML comments

--- Source/Picton/LargeMessageEnvelope.cs
+++ Source/Picton/LargeMessageEnvelope.cs
@@ -3,14 +3,29 @@ using System;
 
 namespace Picton
 {
+	/// <summary>
+	/// Represents an envelope for transporting large messages by referencing their storage location and version
+	/// information.
+	/// </summary>
+	/// <remarks>
+	/// This class is used internally by the Picton library and it's not intended to be used by developers.
+	/// It would make sense to keep this class 'internal' but it needs to be 'public' for MessagePack to be able
+	/// to serialize and deserialize it when running on Linux under .NET full framework.
+	/// </remarks>
 	[MessagePackObject(AllowPrivate = true)]
 	public class LargeMessageEnvelope
 	{
 		#region PROPERTIES
 
+		/// <summary>
+		/// Gets or sets the name of the blob where the content of the message is available.
+		/// </summary>
 		[Key(0)]
-		public string BlobName { get; internal set; }
+		public string BlobName { get; set; }
 
+		/// <summary>
+		/// Gets or sets the version of the Picton library that was used to send the message.
+		/// </summary>
 		[Key(1)]
 		public Version Version { get; set; }
 
--- Source/Picton/MessageEnvelope.cs
+++ Source/Picton/MessageEnvelope.cs
@@ -4,17 +4,34 @@ using System.Collections.Generic;
 
 namespace Picton
 {
+	/// <summary>
+	/// Represents a container for a message, including its version, content, and associated metadata.
+	/// </summary>
+	/// <remarks>
+	/// This class is used internally by the Picton library and it's not intended to be used by developers.
+	/// It would make sense to keep this class 'internal' but it needs to be 'public' for MessagePack to be able
+	/// to serialize and deserialize it when running on Linux under .NET full framework.
+	/// </remarks>
 	[MessagePackObject(AllowPrivate = true)]
 	public class MessageEnvelope
 	{
 		#region PROPERTIES
 
+		/// <summary>
+		/// Gets or sets the version of the Picton library that was used to send the message.
+		/// </summary>
 		[Key(0)]
 		public Version Version { get; set; }
 
+		/// <summary>
+		/// Gets or sets the message content.
+		/// </summary>
 		[Key(1)]
 		public object Content { get; set; }
 
+		/// <summary>
+		/// Gets or sets the collection of metadata associated with the message.
+		/// </summary>
 		[Key(2)]
 		public IDictionary<string, string> Metadata { get; set; }
 
